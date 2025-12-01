4b9e6bbef07ff0bd21820ff7f98b3b150b448a57
AnnaLWilliams <williamsab2718@gmail.com>
2025-12-01 17:16:17 -0500

    in progress solution for day 1 part 2

--- day-1-secret-entrance/secret-entrance.py
+++ day-1-secret-entrance/secret-entrance.py
@@ -2,13 +2,22 @@ dialPos = 50
 zeroCount = 0
 import os
 print (os.getcwd())
-#Debugger Path with open("day-1-secret-entrance/input/input.txt") as fin:
-with open("input/input.txt") as fin:
+#with open("day-1-secret-entrance/input/inputTest.txt") as fin:
+with open("input/inputTest.txt") as fin:
     for line in fin:
-        if line[0] == "L":
-            dialPos = (dialPos - int(line[1:])) % 100
-        elif line[0] == "R":
-            dialPos = (dialPos + int(line[1:])) % 100
-        if dialPos == 0:
+        moveCalc = int(line[1:])
+        while moveCalc >= 100:
             zeroCount += 1
+            moveCalc -= 100
+            
+        if line[0] == "L":
+            dialPos -= int(line[1:])
+            if dialPos < 0:
+                zeroCount += 1
+                dialPos = dialPos % 100
+        elif line[0] == "R":
+            dialPos -= int(line[1:])
+            if dialPos > 0:
+                zeroCount += 1
+                dialPos = dialPos % 100
 print(zeroCount)
