7dbbe547d24ad10c72ec8f0feb3e677745ab9b57
AnnaLWilliams <williamsab2718@gmail.com>
2025-12-01 19:39:19 -0500

    update .gitignore to not include the input files

--- day-1-secret-entrance/secret-entrance.py
+++ day-1-secret-entrance/secret-entrance.py
@@ -2,8 +2,7 @@ dialPos = 50
 zeroCount = 0
 import os
 print (os.getcwd())
-#with open("day-1-secret-entrance/input/inputTest.txt") as fin:
-with open("input/inputTest.txt") as fin:
+with open("day-1-secret-entrance/input/input.txt") as fin:
     for line in fin:
         moveCalc = int(line[1:])
         while moveCalc >= 100:
@@ -11,13 +10,13 @@ with open("input/inputTest.txt") as fin:
             moveCalc -= 100
             
         if line[0] == "L":
-            dialPos -= int(line[1:])
+            dialPos -= moveCalc
             if dialPos < 0:
                 zeroCount += 1
                 dialPos = dialPos % 100
         elif line[0] == "R":
-            dialPos -= int(line[1:])
-            if dialPos > 0:
+            dialPos += moveCalc
+            if dialPos > 99:
                 zeroCount += 1
                 dialPos = dialPos % 100
 print(zeroCount)
